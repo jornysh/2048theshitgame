a635134e191a7429adeed86335cadcd153a33a7f
Jakob Ornysh <jornysh@gmail.com>
2022-12-21 13:03:13 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -31,29 +31,3 @@ fileUploader.onchange = function() {
         addNewFile(files[i].name);
     }
 }
-
-theme = document.getElementById('theme');
-activated = false;
-
-theme.onclick = function () {
-    console.log('Changed theme');
-    activated = !activated;
-    console.log(activated);
-
-    let body = document.querySelector('body');
-    // let input = document.querySelector('.input');
-    // let p = document.querySelector('p');
-    // let list = document.querySelector('.list');
-
-    if (activated) {
-        body.style.background = "linear-gradient(45deg, #4B155C, #471578) no-repeat center center fixed";
-        // input.style.background = "#1C53D6";
-        // p.style.background = "#1C53D6";
-        // list.style.background = "#1C53D6";
-    } else {
-        body.style.background = "linear-gradient(45deg, rebeccapurple, lightcoral) no-repeat center center fixed";
-        // input.style.background = "#456BD9";
-        // p.style.background = "#456BD9";
-        // list.style.background = "#456BD9";
-    }
-}
