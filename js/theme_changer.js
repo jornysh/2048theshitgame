124ecfb130f76444949a97be1a4fa22e64e57c87
Jakob Ornysh <jornysh@gmail.com>
2023-01-12 11:56:04 +0500

    fixed theme changer

--- js/theme_changer.js
+++ js/theme_changer.js
@@ -1,25 +1,43 @@
 theme = document.getElementById('theme');
-activated = false;
-console.log('123')
+activated = (document.cookie === 'theme=dark');
+if (activated) {
+    document.body.classList.remove('light-theme');
+    document.body.classList.add('dark-body');
+    document.cookie = "theme=dark";
+} else {
+    document.body.classList.remove('dark-body');
+    document.body.classList.add('light-body');
+    document.cookie = "theme=light";
+}
+
+console.log(document.cookie);
 theme.onclick = function () {
     console.log('Changed theme');
+    console.log(document.cookie);
     activated = !activated;
     console.log(activated);
 
-    let body = document.querySelector('body');
     // let input = document.querySelector('.input');
     // let p = document.querySelector('p');
     // let list = document.querySelector('.list');
+    console.log(document.body.classList)
 
     if (activated) {
-        body.style.background = "linear-gradient(45deg, #4B155C, #471578) no-repeat center center fixed";
         // input.style.background = "#1C53D6";
         // p.style.background = "#1C53D6";
         // list.style.background = "#1C53D6";
+        document.body.classList.remove('light-body');
+        document.body.classList.add('dark-body');
+
+        document.cookie = "theme=dark";
     } else {
-        body.style.background = "linear-gradient(45deg, rebeccapurple, lightcoral) no-repeat center center fixed";
         // input.style.background = "#456BD9";
         // p.style.background = "#456BD9";
         // list.style.background = "#456BD9";
+        document.body.classList.remove('dark-body');
+        document.body.classList.add('light-body');
+
+        document.cookie = "theme=light";
     }
+    document.body.style.animation = "gradient 15s ease infinite";
 }
